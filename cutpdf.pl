734ac415b529d49e7c02f1d1b9c7d1d1e6f0841d
Kang-min Liu <gugod@gugod.org>
2013-09-12 08:56:51 +0200

    decided to still run through the pre-process to clean some bad pixels.

---
+++ cutpdf.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/env perl
+
+=head1 USAGE
+
+    cutpdf.pl -o /dir/output -r receipt.json foo.pdf
+
+=cut
+
+use v5.14;
+use strict;
+use warnings;
+use FindBin;
+use lib "$FindBin::RealBin/lib";
+
+use File::Path "make_path";
+use File::Basename "basename";
+use Getopt::Std;
+
+my %opts;
+getopts("o:r:", \%opts);
+
+$opts{o} or die "`-o dir/` is required";
+make_path($opts{o}) unless -d $opts{o};
+
+my $pdffile = $ARGV[0] or die;
+my $pdfoutputbase = basename($pdffile, ".pdf", ".PDF");
+my $pdfoutput = $opts{o}."/".$pdfoutputbase."/page";
+make_path($opts{o}."/".$pdfoutputbase);
+
+system("pdftoppm", "-mono", "-r", "300", $pdffile, $pdfoutput);
+
+system "parallel",$^X, "preprosses.pl", "{}", "{.}.png", ":::", <$pdfoutput-*.pbm>;
+system "parallel", $^X, "cuttext.pl", "-o", "{.}", "-r", "{.}/receipt.json", "{}", ":::", <$pdfoutput-*.png>;
