a6f95e6bd11f58d750d12ae125d222a4027f524c
Kang-min Liu <gugod@gugod.org>
2013-09-18 17:13:18 +0200

    use netpbm to help doing deskew and deshadow.

--- preprocess.pl
+++ preprocess.pl
@@ -26,13 +26,14 @@ my ($file_base, $file_suffix) = $file =~ m!\A(.+)\.([^.]+)\z!;
 
 my $angle = `pamtilt $origin_file_name` =~ s/\n\z//r;
 
-system("pgmdeshadow $origin_file_name | pnmrotate $angle > $dir/${file_base}.deshadow.deskew.pnm");
+system("pgmdeshadow $origin_file_name | pnmrotate -background '#fff' $angle > $dir/${file_base}.deshadow.deskew.pgm");
 
-my $img = Imager->new( file => "$dir/${file_base}.deshadow.deskew.pnm" );
+my $img = Imager->new( file => "$dir/${file_base}.deshadow.deskew.pgm" );
 $img = $img->convert( preset => "grey" );
+$img->write( file => $ARGV[1] );
 
-my $mama = ImageMama->new( image =>  $img );
-$mama->cut_margin;
-$mama->clean_outlier_pixels3;
+# my $mama = ImageMama->new( image =>  $img );
+# $mama->cut_margin;
+# $mama->clean_outlier_pixels3;
 
-$mama->image->write( file => $ARGV[1] );
+# $mama->image->write( file => $ARGV[1] );
