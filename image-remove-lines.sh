fec86239a553a7b3d296c3dc885b33557af23697
Kang-min Liu <gugod@gugod.org>
2013-10-11 20:10:58 +0200

    improved line-removal.

--- image-remove-lines.sh
+++ image-remove-lines.sh
@@ -8,12 +8,12 @@ line_length=100
 image_width=$(convert ${input_file} -format '%[fx:w]' info:)
 image_height=$(convert ${input_file} -format '%[fx:h]' info:)
 
-hline_length=$(perl -e "print( 1+int($image_width * .1) )")
-vline_length=$(perl -e "print( 1+int($image_height *.1) )")
+hline_length=$(perl -e "print( 1+int($image_width  * .1) )")
+vline_length=$(perl -e "print( 1+int($image_height * .1) )")
 
-echo $image_width x $image_height : $hline_length , $vline_length
+echo $image_width x $image_height : $hline_length , $vline_length : $(($hline_length * 2 )), $(( $vline_length * 2 ))
 
 convert ${input_file} \
-  \( +clone -statistic mode 1x${hline_length} \) -compose Minus -composite -negate \
-  \( +clone -statistic mode ${vline_length}x1 \) -compose Minus -composite -negate \
+  \( +clone -statistic mode 1x${hline_length} -statistic mode 1x$(( ${hline_length} * 2 ))  -statistic minimum 3x1 \) -compose Minus -composite -negate \
+  \( +clone -statistic mode ${vline_length}x1 -statistic mode $(( ${vline_length} * 2 ))x1  -statistic minimum 1x3 \) -compose Minus -composite -negate \
 ${output_file}
